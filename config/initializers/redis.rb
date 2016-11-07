b0037362269a41a79ee652da07745a3c89876966
Raymond Cohen <raycohen@gmail.com>
2016-11-06 22:27:04 -0500

    configure redis to read the heroku redistogo env variable

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,6 @@
-uri = URI.parse(ENV["DEVOCLOCK_REDIS_TOGO_URL"])
+if Rails.env.production?
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+else
+  uri = URI.parse(ENV["DEVOCLOCK_REDIS_TOGO_URL"])
+end
 $redis = Redis.new(:url => uri)
