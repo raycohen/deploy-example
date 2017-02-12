8149400b838be6aa80f8147b96712d0cc5918dc2
Raymond Cohen <raycohen@gmail.com>
2017-02-12 12:54:49 -0500

    add react controller

---
+++ app/controllers/react_controller.rb
@@ -0,0 +1,44 @@
+class ReactController < ApplicationController
+  def index
+    active_html = get_activated_html_from_redis
+    inject_meta_tag(active_html)
+    render text: active_html
+  end
+
+  private
+
+  def get_activated_html_from_redis
+    version_key = version_from_params || 'current-content'
+    $redis.get("react-deployer:index:#{version_key}")
+  end
+
+  def get_active_version
+    version_from_params or $redis.get("react-deployer:index:current")
+  end
+
+  def version_from_params
+    version = params[:version]
+    version if version.present? && version.length == 32
+  end
+
+  def demo_data
+    {
+      :version => get_active_version
+    }
+  end
+
+  def escaped_demo_data_json
+    Rack::Utils.escape_html(demo_data.to_json)
+  end
+
+  def inject_meta_tag(html)
+    html.insert(inside_head_tag_position(html), <<-HTML.strip)
+      <meta name="demo-data" content="#{escaped_demo_data_json}">
+    HTML
+  end
+
+  def inside_head_tag_position(html)
+    head_tag_position = html.index("<head")
+    html.index(">", head_tag_position) + 1
+  end
+end
